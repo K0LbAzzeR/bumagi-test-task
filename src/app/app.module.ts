8ee041ad645400d14d04c89ffa42a204f2e69889
K0Lb@zzeR <javasc@yandex.ru>
2019-03-12 13:18:08 +0700

    add CustomMaterialModuleModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+
+import { CustomMaterialModuleModule } from './custom-material-module/custom-material-module.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    CustomMaterialModuleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
